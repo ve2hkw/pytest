61ddecac0fc7541ee0f8153099a8b4cd6c5d3012
harrison <ve2hkw@gmail.com>
2016-07-27 07:35:07 -0400

    A journey of a thousand miles begins with a single commit, or wait, that is wrong.... Anyway, this is an initial commmit just to get the ball rolling for version control

---
+++ pytest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#Pytest is an amateur radio contest logger that is desiged
+#to be easily extended for various contests with simple config
+#files. Hopefully.
+
+import curses
+
